ca0786d334f4287c4ededdff6c3acef56a9c03bb
Igor Maravic <iggy@Iggy-Is.local>
2022-10-10 02:41:51 -0400

    game constructor

--- Front_End_Game/lib/script.js
+++ Front_End_Game/lib/script.js
@@ -1,28 +1,34 @@
-// I'm going to need a random word generator
-// text field connected to dictionary
-// const url = "https://random-word-api.herokuapp.com/word"
 const url = "https://random-word-api.herokuapp.com/word";
-let div = document.createElement("div");
-let wordGenerator = document.querySelector(".wordButton");
+let wordGenerator = document.querySelector(".wordbutton");
 wordGenerator.addEventListener("click", handleRandomWord);
-document.body.appendChild(div);
-
-// split method used to break a word 
-// 1:1 comparison to check against letters 
-// keyboard event listener
-
 
 
 function handleRandomWord() {
   fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data[0].split(""))
-      
+  .then(res => res.json())
+  .then(data => {
+    let splitWord = data[0].split("")
+    
+    console.log(splitWord)
+    //--------------------SPLITTING THE WORD-------------------- 
+    for (let i = 0; i < splitWord.length; i++) {
+      console.log(splitWord[i])
+      let div = document.createElement("div");
+      div.classList.add("randomword")
+      div.style.width = '80px'
+      div.style.height = '20px'
+      div.innerHTML = splitWord[i]
+      // below works just fine
+      document.body.appendChild(div);
+    }
     })
-  
   }
-  console.log(wordGenerator.innerHTML)
+// -------------------KEYBOARD EVENT-----------------------------
+
+
+
+
+
 
 
 
@@ -31,4 +37,15 @@ function handleRandomWord() {
 //     console.log(numOfLetters);
 //   }
 
-// randomLetters()
+// randomLetters(1, 5)
+
+// ---------------GAME CONSTRUCTOR-------------
+
+player = function () {
+  wordHold = document.querySelector("wordhold");
+  positive = document.createElement("ul");
+}
+
+for (let i = 0; i < word.length; i++) {
+
+}
