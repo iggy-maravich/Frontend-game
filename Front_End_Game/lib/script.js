752c7a46c9eb8480e90d19b594eec6c33486c270
Igor Maravic <iggy@Iggy-Is.local>
2022-10-11 00:03:18 -0400

    so far so good

--- Front_End_Game/lib/script.js
+++ Front_End_Game/lib/script.js
@@ -1,51 +1,41 @@
 const url = "https://random-word-api.herokuapp.com/word";
 let wordGenerator = document.querySelector(".wordbutton");
 wordGenerator.addEventListener("click", handleRandomWord);
-
+let wordHold = document.querySelector("wordhold");
+let positive = document.createElement("ul");
+let guess = document.createElement("li");
+let strikes = [];
+strikes.push(guess);
 
 function handleRandomWord() {
   fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    let splitWord = data[0].split("")
+    .then(res => res.json())
+    .then(data => {
+      let splitWord = data[0].split("")
     
-    console.log(splitWord)
-    //--------------------SPLITTING THE WORD-------------------- 
-    for (let i = 0; i < splitWord.length; i++) {
-      console.log(splitWord[i])
-      let div = document.createElement("div");
-      div.classList.add("randomword")
-      div.style.width = '80px'
-      div.style.height = '20px'
-      div.innerHTML = splitWord[i]
-      // below works just fine
-      document.body.appendChild(div);
-    }
+      console.log(splitWord)
+   
+   
+      //--------------------SPLITTING THE WORD--------------------
+      for (let i = 0; i < splitWord.length; i++) {
+        console.log(splitWord[i])
+        let div = document.createElement("div");
+        div.classList.add("randomword")
+        div.style.width = '80px'
+        div.style.height = '20px'
+        div.innerHTML = splitWord[i]
+        document.body.appendChild(div);
+      }
     })
+    
+  // ---------------GAME CONSTRUCTOR-------------
+  
+  
+  player = function () {
+    if (splitWord[i] === "-") {
+      wordHold.innerHTML = "-";
+    } else {
+      
+    }
   }
-// -------------------KEYBOARD EVENT-----------------------------
-
-
-
-
-
-
-
-
-//   function randomLetters(min, max) {
-//     let numOfLetters = max - min + 1;
-//     console.log(numOfLetters);
-//   }
-
-// randomLetters(1, 5)
-
-// ---------------GAME CONSTRUCTOR-------------
-
-player = function () {
-  wordHold = document.querySelector("wordhold");
-  positive = document.createElement("ul");
-}
-
-for (let i = 0; i < word.length; i++) {
-
 }
